940865d46738ebed93dfb1862e1cf2b127918188
zevt <vietsemantics@gmail.com>
2018-05-07 19:52:36 -0500

    init first code

---
+++ src/main/java/com/zeroexception/common/bamboo/BambooApplication.java
@@ -0,0 +1,12 @@
+package com.zeroexception.common.bamboo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BambooApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BambooApplication.class, args);
+  }
+}
